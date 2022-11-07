9d76405567bf09afe86866e11d0ea42ca682e54d
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-07 19:55:07 -0300

    Estilização básica do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import React from 'react'
 import * as C from './App.styles'
 
 const App = () => {
   return(
     <C.Container>
-
+      <C.Area>
+        <C.Header>
+          Lista de Tarefas
+        </C.Header>
+      </C.Area>
     </C.Container>
   )
 }
