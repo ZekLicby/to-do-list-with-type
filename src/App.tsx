665531b5ca54cb5105a5c1e0a18e7a9b3d2ef223
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-07 20:52:27 -0300

    Criação da pasta de componentes e do primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import * as C from './App.styles'
+import {Item} from './types/item'
+import {ListItem} from './components/ListItem'
 
 const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Finalizar esse projeto aqui", done: false},
+    { id: 2, name: "ablublebe", done: false},
+  ])
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>
           Lista de Tarefas
         </C.Header>
+
+        {/* Área de adição de tarefas */}
+
+        <ul>
+          {list.map((item, index) => (
+            <ListItem/>
+          ))}
+        </ul>
+
       </C.Area>
     </C.Container>
   )
